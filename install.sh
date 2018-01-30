172feb2f2989836e0934f7077993b4e8896198d1
Rob Schuler <robes@users.noreply.github.com>
2018-01-30 15:53:31 -0800

    fix typo in install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 JBROWSE=${1:-"/var/www/html/jbrowse/latest"}
 JBROWSE_CONF="${JBROWSE}/jbrowse.conf"
-cp -R ./Facebase "${JBROWSE}/plugin"
+cp -R ./Facebase "${JBROWSE}/plugins"
 if [ -f "${JBROWSE_CONF}" ]
 then
     mv "${JBROWSE_CONF}" "${JBROWSE_CONF}-$(date +%Y%m%d-%H%M%S)"
