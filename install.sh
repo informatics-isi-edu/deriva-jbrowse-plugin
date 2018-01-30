1b303f6abc09f37959d68e6644e12fbda0f13748
Rob Schuler <robes@users.noreply.github.com>
2018-01-30 15:13:53 -0800

    added install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+JBROWSE=${1:-"/var/www/html/jbrowse/latest"}
+JBROWSE_CONF="${JBROWSE}/jbrowse.conf"
+cp -R ./Facebase "${JBROWSE}/plugin"
+if [ -f "${JBROWSE_CONF}" ]
+then
+    mv "${JBROWSE_CONF}" "${JBROWSE_CONF}-$(date +%Y%m%d-%H%M%S)"
+fi
+cp ./jbrowse.conf "${JBROWSE_CONF}"
+
